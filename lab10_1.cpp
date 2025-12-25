4aa5db1e3b8fe8a300722f157d19bed124ead398
shokun2349 <shokun2349@gmail.com>
2025-12-26 02:54:25 +0700

    lab10_1 complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,27 +1,47 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
-	}while(true);
-	
-	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
-	
-	return 0;
+int main() {
+    char grade;
+    int countA = 0, countB = 0, countC = 0, countD = 0, countF = 0;
+    int studentCount = 1;
+
+    cout << "Please input grade of each student (A-F) or input 0 to exit." << endl;
+
+    while (true) {
+        cout << "Student [" << studentCount << "]: ";
+        cin >> grade;
+
+        if (grade == '0') {
+            break; 
+        }
+
+        if (grade == 'A' || grade == 'a') {
+            countA++;
+            studentCount++;
+        } else if (grade == 'B' || grade == 'b') {
+            countB++;
+            studentCount++;
+        } else if (grade == 'C' || grade == 'c') {
+            countC++;
+            studentCount++;
+        } else if (grade == 'D' || grade == 'd') {
+            countD++;
+            studentCount++;
+        } else if (grade == 'F' || grade == 'f') {
+            countF++;
+            studentCount++;
+        } else {
+            cout << "Wrong input. Please input again." << endl;
+        }
+    }
+
+    int totalStudents = studentCount - 1;
+    cout << "In total " << totalStudents << " students." << endl;
+    cout << "A = " << countA << ", B = " << countB << ", C = " << countC 
+         << ", D = " << countD << ", F = " << countF << endl;
+
+    return 0;
 }
